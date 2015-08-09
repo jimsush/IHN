7aa9170df0dc6d3d33e2bbca2d6903067dae3b3d
jimsush <jimsu123@gmail.com>
2015-08-09 23:42:25 +0800

    refactor class name, add maven support

---
+++ IHNServer/src/main/java/com/ihn/server/util/SysUtil.java
@@ -0,0 +1,18 @@
+package com.ihn.server.util;
+
+public class SysUtil {
+
+	public static void printException(Throwable th){
+		
+	}
+	
+	public static void runCmdInWindows(){
+		
+	}
+	
+	public static void runCmdInLinux(){
+		
+	}
+	
+	
+}
---
+++ IHNServer/support/mysql/run-mysql.bat
@@ -0,0 +1,16 @@
+
+title Mysql5.0.20a for OpView V2.0 Console
+
+set DIRNAME=.\
+if "%OS%" == "Windows_NT" set DIRNAME=%~dp0%
+pushd %DIRNAME%
+
+REM start mysql using configure file
+set MYSQL_HOME=.
+set MYSQL_CONF_FILE=ihn.ini
+
+REM if you want to start mysql from command and without config file,
+REM use following command, BTW, please delete the inno db log file at first.
+rem %MYSQL_HOME%\bin\mysqld --verbose --default-character-set=GBK --default-storage-engine=INNODB
+
+%MYSQL_HOME%\bin\mysqld --defaults-file="%MYSQL_CONF_FILE%" --verbose --default-storage-engine=INNODB
