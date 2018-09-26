26a2b8bf06b1825cbb18521151ed704e8b254c0a
jimsush <jimsu123@gmail.com>
2018-09-26 14:44:51 +0800

    Create py1.py

---
+++ projects/support/py1.py
@@ -0,0 +1,13 @@
+import numpy
+
+a=[3.5, 2.0, 0,  4.5,5.0, 1.5, 2.5, 2.0]  //user a
+b=[2.0, 3.5, 4.0,0,  2.0, 3.5, 0,   3.0]  //user b
+suma=sum([i*i for i in a])
+sqrta=numpy.sqrt(suma)        //user a的值
+
+sumb=sum([i*i for i in b])
+sqrtb=numpy.sqrt(sumb)
+
+dotab=numpy.dot(a,b)  //  a.b
+
+cos1=dotab/(sqrta*sqrtb) //cos相似度, =1最相似
